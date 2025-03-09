37b387971097a714edd950dc139074f0b456351c
Chauhan-1999 <tarunchauhan798528@gmail.com>
2025-03-09 16:15:45 +0530

    Add Initial Files

---
+++ src/main/java/com/tarunproject/journal/App/controller/HealthCheck.java
@@ -0,0 +1,14 @@
+package com.tarunproject.journal.App.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/health-check")
+    public String healthCheck(){
+        return "Ok";
+    }
+}
+
